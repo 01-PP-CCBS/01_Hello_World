c655c44d2148b633a0921c29aafe863603a9b9af
jan <jaco@emailn.de>
2024-05-22 09:05:27 +0200

    sys out test

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,10 +1,11 @@
 package com.cc.java;
 
-public class App 
+public class App
 {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Tes1t");
+        System.out.println("check");
     }
     
 }
